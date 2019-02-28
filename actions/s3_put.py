561b18e35e472cff850c4da2159c7a2535954eb3
yaleman <yaleman@ricetek.net>
2019-03-01 07:56:39 +1000

    changing to s3 verbs

--- actions/s3_put.py
+++ actions/s3_put.py
@@ -4,7 +4,7 @@ if __name__ != '__main__':
     from st2common.content import utils
     from st2common.runners.base_action import Action
 
-class S3_upload(Action):
+class S3_put(Action):
     def run(self, **args):
         """ uploads a file from the local disk """
         return "Hello world"
