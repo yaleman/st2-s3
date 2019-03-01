5f37240db0a6a1f52fbac53844d22c912bba1327
yaleman <yaleman@ricetek.net>
2019-03-01 11:08:05 +1000

    let's see if this'll work

--- actions/s3_put.py
+++ actions/s3_put.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 
+from minio import Minio
+from minio.error import (ResponseError, BucketAlreadyOwnedByYou,
+                         BucketAlreadyExists)
+
+
 if __name__ != '__main__':
     from st2common.content import utils
     from st2common.runners.base_action import Action
@@ -7,4 +12,24 @@ if __name__ != '__main__':
 class S3_put(Action):
     def run(self, **args):
         """ uploads a file from the local disk """
-        return "Hello world"
+        # Initialize minioClient with an endpoint and access/secret keys.
+        minioClient = Minio(endpoint,
+                            access_key=access_key,
+                            secret_key=secret_key,
+                            secure=secure)
+
+        # Make a bucket with the make_bucket API call.
+        try:
+            minioClient.make_bucket(bucket)
+        except BucketAlreadyOwnedByYou as err:
+            pass
+        except BucketAlreadyExists as err:
+            pass
+        except ResponseError as err:
+            raise
+        else:
+            try:
+                minioClient.fput_object(bucket, filename, filesource)
+                return True
+            except ResponseError as err:
+                print(err)
