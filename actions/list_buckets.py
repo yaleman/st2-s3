9732097b06f036c10de8ccc2dca35448c9e08085
yaleman <yaleman@ricetek.net>
2019-03-02 13:44:07 +1000

    adding more functions

---
+++ actions/list_buckets.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from minio.error import (ResponseError, BucketAlreadyOwnedByYou,
+                         BucketAlreadyExists)
+
+from client import minio_client
+
+if __name__ != '__main__':
+    from st2common.content import utils
+    from st2common.runners.base_action import Action
+
+class S3_list_buckets(Action):
+    def run(self, endpoint_secure=True, endpoint=None, access_key=None, secret_key=None, bucket=None):
+        """ lists available buckets """
+
+        # Initialize minioClient with an endpoint and access/secret keys.
+        success, minioClient = minio_client(self, endpoint=endpoint, endpoint_secure=endpoint_secure, access_key=access_key, secret_key=secret_key)
+
+        if not success:
+            return (False, minioClient)
+
+        try:
+            return (True, minioClient.list_buckets())
+        except ResponseError as err:
+            return (False, err)
---
+++ actions/make_bucket.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from minio.error import (ResponseError, BucketAlreadyOwnedByYou,
+                         BucketAlreadyExists)
+
+from client import minio_client
+
+if __name__ != '__main__':
+    from st2common.content import utils
+    from st2common.runners.base_action import Action
+
+class S3_make_bucket(Action):
+    def run(self, endpoint_secure=True, endpoint=None, access_key=None, secret_key=None, bucket=None):
+        """ makes a bucket """
+
+        # Initialize minioClient with an endpoint and access/secret keys.
+        success, minioClient = minio_client(self, endpoint=endpoint, endpoint_secure=endpoint_secure, access_key=access_key, secret_key=secret_key)
+
+        if not success:
+            return (False, minioClient)
+
+        try:
+            if minioClient.bucket_exists(bucket):
+                return (False, "Bucket {} already exists".format(bucket))
+            else:
+                return (True, minioClient.make_bucket(bucket))
+        except ResponseError as err:
+            return (False, err)
